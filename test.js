78a417b70c952364bfb04a12583540beb285abae
Muceres <samuel.joset@gmail.com>
2020-09-16 02:17:14 +0200

    fix: test

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ const koaHandler = require(".");
 
 describe("koaHandler tests", () => {
   test("if koaHandler is called then it must be defined", () => {
-    expect(typeof dummyFunction).toBe("function");
+    expect(typeof koaHandler).toBe("function");
   });
 });
